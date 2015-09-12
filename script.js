ee0bda65e0cbb4f409b914a0ac3e355e11a69105
Danny Libin <omegasol11@gmail.com>
2015-09-12 02:13:05 -0500

    Implemented caching to improve responsiveness, design tweaks

--- script.js
+++ script.js
@@ -1,12 +1,13 @@
 var $quote;
-var $tweetButton;
+var quoteCache = [];
+var cacheSize = 6;
 
-function getQuote() {
+function initialQuote() {
     
     /*Response format:
     {
       "type": "success",
-      "value": {
+      "value": {      <-- As list of objects for multiple
         "id": 458,
         "joke": "Chuck Norris can write infinite recursion functions and have them return.",
         "categories": [
@@ -16,12 +17,49 @@ function getQuote() {
     }
     */
     $.ajax({
-        url: "http://api.icndb.com/jokes/random?limitTo=[nerdy]"+"&callback=?",
+        url: "http://api.icndb.com/jokes/random?limitTo=[nerdy]" + "&callback=?",
         method: "GET",
         dataType: "jsonp",
         success: function (result) {
             if (result.type == "success") {
                 $quote.html(result.value.joke);
+            }
+        },
+        error: function (xhr, textStatus, errorThrown) {
+            console.log("readyState: " + xhr.readyState);
+            console.log("responseText: " + xhr.responseText);
+            console.log("status: " + xhr.status);
+            console.log("text status: " + textStatus);
+            console.log("error: " + errorThrown);
+        }
+    });
+}
+
+function getQuote() {
+    if (quoteCache.length > 0) {
+        console.log(quoteCache.length);
+        $quote.html(quoteCache.shift());
+    } else {
+        cacheQuotes(cacheSize);
+        initialQuote();
+    }
+    
+    if (quoteCache.length < 6) {
+        var amountToCache = cacheSize - quoteCache.length;
+        cacheQuotes(amountToCache);
+    }
+}
+
+function cacheQuotes(numQuotes) {
+    $.ajax({
+        url: "http://api.icndb.com/jokes/random/" + numQuotes + "?limitTo=[nerdy]" + "&callback=?",
+        method: "GET",
+        dataType: "jsonp",
+        success: function (result) {
+            if (result.type == "success") {
+                for (var i = 0; i < result.value.length; i++) {
+                    quoteCache.push(result.value[i].joke);
+                }
             }
         },
         error: function(xhr, textStatus, errorThrown) {
@@ -36,10 +74,11 @@ function getQuote() {
 
 $(document).ready(function() {
     $quote = $("#quote-box");
-    $tweetButton = $("#tweet-button");
+    var $tweetButton = $("#tweet-button");
     
-    // Get a quote to start.
-    getQuote();
+    // Get a quote to start and load a cache for better response.
+    initialQuote();
+    cacheQuotes(cacheSize);
     
     $("#generate").click(function () {
         getQuote();
